ce2e7ec1e634cfb1e6353752678befacda890a4a
black <black@LAPTOP-1HCFCA7E>
2022-12-13 21:18:08 +0500

    Добавьте файлы проекта.

---
+++ FP C++ 123/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iomanip>
+#include <format>
+#include <array>
+#include <chrono>
+#include <string>
+#include <clocale>
+#include <Windows.h>
+#include <random>
+#include <fstream>
+using namespace std;
+
+
+int main() {
+	
+}
+
